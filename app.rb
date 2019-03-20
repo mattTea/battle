0aa6879879a05807e385adde3810bded893c60aa
Matt Thompson <matt.thompson@johnlewis.co.uk>
2019-03-20 15:09:33 +0000

    Refactors last step to redirect to GET route to displat player names

--- app.rb
+++ app.rb
@@ -2,13 +2,22 @@ require 'sinatra/base'
 
 class Battle < Sinatra::Base
   
+  enable :sessions
+
   get "/" do
     # "Testing infrastructure working!"
     erb(:index)
   end
 
   post "/names" do
-    params
+    session[:player1] = params[:player_1_name]
+    session[:player2] = params[:player_2_name]
+    redirect("/play")
+  end
+
+  get "/play" do
+    @player1 = session[:player1]
+    @player2 = session[:player2]
     erb(:play)
   end
 
